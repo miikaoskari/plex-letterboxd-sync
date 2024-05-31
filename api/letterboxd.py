cd0f52617625cf95d2becb18156dc26b667ed7dc
miikaoskari <miika.karjalainen13@gmail.com>
2024-05-31 22:44:38 +0300

    feat: Add film ID extraction from HTML content

--- api/letterboxd.py
+++ api/letterboxd.py
@@ -7,6 +7,7 @@ class Letterboxd:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.film_count = 0
 
     def login(self):
         self.session = requests.Session()
@@ -41,6 +42,17 @@ class Letterboxd:
             "importFilms": data,
         }
         return json.dumps(wrapped_data)
+    
+    def find_film_ids(self, content):
+        soup = bs4.BeautifulSoup(content, 'html.parser')
+        film_elements = soup.find_all("div")
+        film_ids = []
+        for film in film_elements:
+            film_id = film.get("data-film-id")
+            if film_id:
+                film_ids.append(("importFilmId", film_id))
+                self.film_count += 1
+        return film_ids
 
     def import_data(self, file):
         try:
@@ -63,27 +75,38 @@ class Letterboxd:
         data_json_decoded = self.parse_film_jsons(result.content)
         
         data = {
+            "json": data_json_decoded,
             "__csrf": self.csrf,
-            "data": data_json_decoded,
         }
         try:
             result = self.session.post("https://letterboxd.com/import/watchlist/match-import-film/", data=data)
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
         
-        data = {
-            "__csrf": self.csrf,
-            "filmListId": "",
-            "name": "",
-            "publicList": "",
-            "numberedList": "",
-            "notes": "",
-            "tags": "",
-            "shouldMarkAsWatched": "true",
-            "shouldImportWatchedDates": "true",
-            "importRating": "",
-            
-        }
+
+        film_ids = self.find_film_ids(result.content)
+
+        default_data = [
+            ("__csrf", self.csrf),
+            ("filmListId", ""),
+            ("name", ""),
+            ("publicList", ""),
+            ("numberedList", ""),
+            ("notes", ""),
+            ("tags", ""),
+            ("shouldMarkAsWatched", "true"),
+            ("shouldImportWatchedDates", "true"),
+        ]
+
+        import_ratings = [("importRating", "") for _ in range(self.film_count)]
+        import_reviews = [("importReview", "") for _ in range(self.film_count)]
+        import_tags = [("importTags", "") for _ in range(self.film_count)]
+        import_watched_dates = [("importWatchedDate", "") for _ in range(self.film_count)]
+        import_rewatch = [("importRewatch", "false") for _ in range(self.film_count)]
+        default_data.append(film_ids)
+        import_viewing_ids = [("importViewingId", "") for _ in range(self.film_count)]
+        should_import_films = [("shouldImportFilm", "true") for _ in range(self.film_count)]
+
         try:
             self.session.post("https://letterboxd.com/s/save-users-imported-imdb-history/")
         except requests.exceptions.RequestException as e:
