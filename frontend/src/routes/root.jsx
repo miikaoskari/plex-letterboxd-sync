3f47a0b53840317ecd8e3b3997fcfabe65261290
miikaoskari <miika.karjalainen13@gmail.com>
2024-06-01 22:33:34 +0300

    Add landing page components and sync button

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,6 +1,16 @@
+import {ProgressBar} from "../components/ProgressBar";
+import {Button} from "../components/Button";
+
 export default function Root() {
   return (
     <>
+    <div className={"flex justify-center m-4"}>
+      <h1 className={"text-4xl font-bold text-white mt-4 text-center"}>Plex-Letterboxd-Sync</h1>
+    </div>
+    <div className={"flex justify-center mt-12"}>
+        <Button className={"mr-4"}>Start Sync</Button>
+        <ProgressBar className={""} />
+    </div>
     </>
   );
 }
