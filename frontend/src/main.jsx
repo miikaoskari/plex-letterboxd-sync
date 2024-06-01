030b6a9089af98bc6bb538a0c53ef8bc58e9a1eb
miikaoskari <miika.karjalainen13@gmail.com>
2024-06-01 17:52:51 +0300

    add frontend skeleton to project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
+import Login from './routes/login';
+import ErrorPage from './routes/error-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ frontend/src/routes/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../components/Button'
+
+const login = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default login
