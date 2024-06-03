1fd5657ef63adb785ec63520b7d79cb139b20bf2
miikaoskari <miika.karjalainen13@gmail.com>
2024-06-03 21:57:37 +0300

    format all python code

--- sync.py
+++ sync.py
@@ -2,19 +2,51 @@ from api.tautulli_api import Tautulli
 from api.letterboxd import Letterboxd
 import json
 
+
 def read_keys():
-    with open('keys.json') as f:
+    """
+    Reads the keys from the 'keys.json' file and returns them.
+
+    Returns:
+        dict: A dictionary containing the keys.
+    """
+    with open("keys.json") as f:
         keys = json.load(f)
     return keys
 
-def debug():
-    import os
-    # for debugging with burp suite
-    os.environ["REQUESTS_CA_BUNDLE"]="cert.pem"
-    os.environ["HTTP_PROXY"]="http://127.0.0.1:8080"
-    os.environ["HTTPS_PROXY"]="http://127.0.0.1:8080"
 
-def start_sync():
+def debug():
+    """
+    Set up environment variables for debugging with Burp Suite.
+
+    This function sets the following environment variables:
+    - REQUESTS_CA_BUNDLE: Path to the certificate file for SSL/TLS verification.
+    - HTTP_PROXY: URL of the HTTP proxy server.
+    - HTTPS_PROXY: URL of the HTTPS proxy server.
+
+    These environment variables are commonly used when debugging network requests
+    with Burp Suite, a popular web application security testing tool.
+
+    Note: Make sure to replace the values with the appropriate paths and URLs
+    for your specific debugging setup.
+    """
+    import os
+
+    os.environ["REQUESTS_CA_BUNDLE"] = "cert.pem"
+    os.environ["HTTP_PROXY"] = "http://127.0.0.1:8080"
+    os.environ["HTTPS_PROXY"] = "http://127.0.0.1:8080"
+
+
+def start_sync(websocket=None):
+    """
+    Starts the synchronization process between Tautulli and Letterboxd.
+
+    Args:
+        websocket: Optional WebSocket connection.
+
+    Returns:
+        None
+    """
     keys = read_keys()
 
     tautulli = Tautulli(url=keys["tautulli"]["url"], key=keys["tautulli"]["api_key"])
@@ -22,11 +54,14 @@ def start_sync():
     tautulli.check_watched_status()
     tautulli.compile_json_to_csv(file="history")
 
-    letterboxd = Letterboxd(keys["letterboxd"]["username"], keys["letterboxd"]["password"])
+    letterboxd = Letterboxd(
+        keys["letterboxd"]["username"], keys["letterboxd"]["password"]
+    )
     letterboxd.login()
     letterboxd.import_data(file="history.csv")
     letterboxd.match_import_film()
     letterboxd.save_users_imported_imdb_history()
+
 
 if __name__ == "__main__":
     start_sync()
